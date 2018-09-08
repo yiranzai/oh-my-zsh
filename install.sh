519e8ce9c113ec0e5c89cae2ab6fee43128a55dc
yiranzai <duanzx@codepku.com>
2018-09-08 22:04:10 +0800

    添加powerline fonts

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 cp -R $DIR/vim $HOME/.vim
 cp $DIR/vim/.vimrc  $HOME/.vimrc
 cp $DIR/.zshrc  $HOME/.zshrc
+sh $HOME/.oh-my-zsh/fonts
 vim +PluginInstall +qall
 echo " -----------------------------warning-------------------------"
 echo "|   The installation is successful, please execute            |"
